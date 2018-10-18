1b939f143913e05a46bf6d817ee853553a0e4762
Brin <747127514@qq.com>
2018-10-18 17:05:07 +0800

    添加工厂类

---
+++ app/src/main/java/com/brins/weatherdemo/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.brins.weatherdemo.util;
+
+import java.lang.ref.SoftReference;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+
+    static void sendRequest(String address,okhttp3.Callback callback){
+
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+
+    }
+}
---
+++ app/src/main/java/com/brins/weatherdemo/util/Utility.java
@@ -0,0 +1,100 @@
+package com.brins.weatherdemo.util;
+
+import android.support.annotation.NonNull;
+
+import com.brins.weatherdemo.db.Cities;
+import com.brins.weatherdemo.db.Countries;
+import com.brins.weatherdemo.db.Provinces;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.crypto.Cipher;
+
+public class Utility {
+
+    /**
+     * Handle province boolean.
+     *
+     * @param response 解析处理服务器省份响应数据，并添加到省级表
+     * @return the boolean
+     */
+    public static boolean handleProvince(@NonNull String response){
+
+        if (!response.isEmpty()){
+
+            try {
+                JSONArray provincearray=new JSONArray(response);
+                for (int i=0;i<provincearray.length();i++){
+                    JSONObject provincesJSONObject=provincearray.getJSONObject(i);
+                    Provinces provinces=new Provinces();
+                    provinces.setProvinceName(provincesJSONObject.getString("name"));
+                    provinces.setProvinceCode(provincesJSONObject.getInt("id"));
+                    provinces.save();
+                }
+                return true;
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return false;
+
+    }
+
+    /**
+     * Handle city boolean.
+     *
+     * @param response the response 解析处理服务器市级数据,并添加到市级表
+     * @return the boolean
+     */
+    public static boolean handleCity(@NonNull String response,int provinceId){
+
+        if (!response.isEmpty()){
+            try {
+                JSONArray cityarray = new JSONArray(response);
+                for (int i=0;i<cityarray.length();i++){
+                    JSONObject cityobject=cityarray.getJSONObject(i);
+                    Cities cities=new Cities();
+                    cities.setCityName(cityobject.getString("name"));
+                    cities.setCityCode(cityobject.getInt("id"));
+                    cities.setProvinceId(provinceId);
+                    cities.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Handle country boolean.
+     *
+     * @param response the response 解析处理服务器县级数据,并添加到县级表
+     * @return the boolean
+     */
+    public static boolean handleCountry(@NonNull String response,int cityId){
+
+        if (!response.isEmpty()){
+            try {
+                JSONArray countryarray = new JSONArray(response);
+                for (int i=0;i<countryarray.length();i++){
+                    JSONObject countryobject=countryarray.getJSONObject(i);
+                    Countries countries=new Countries();
+                    countries.setCountryName(countryobject.getString("name"));
+                    countries.setWeatherId(countryobject.getInt("id"));
+                    countries.setCityId(cityId);
+                    countries.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
